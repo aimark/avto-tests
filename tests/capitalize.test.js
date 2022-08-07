ae7412fea44ca05c7930afd315a059190f109d61
Андрей Марк <markandreym@gmail.com>
2022-08-07 06:39:16 +0300

    Added assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,14 @@
 import capitalize from "../src/capitalize.js";
+import { strict as assert } from 'assert';
 
-if (capitalize('hello') !== 'Hello') {
+/* if (capitalize('hello') !== 'Hello') {
     throw new Error('Функция работает неверно!');
   }
   
   if (capitalize('') !== '') {
     throw new Error('Функция работает неверно!');
-  }
+  } */
   
-  console.log('Все тесты пройдены!');
+assert.strictEqual(capitalize('hello'), 'Hello'); 
+assert.strictEqual('', ''); 
+console.log('Все тесты пройдены!');
